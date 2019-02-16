d1cf2ef90c4decc0bd6d2c61bb62a8d3b3d4b5e8
BhavyanshM <bm37@students.uwf.edu>
2019-02-16 03:50:21 -0600

    Fix some issues and add the shape.py

--- game.py
+++ game.py
@@ -13,17 +13,19 @@ def click_pos(event, x, y, flags, param):
 cap = cv2.VideoCapture(0);
 
 while True:
-	cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+	# cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 	global frame
 	ret, frame = cap.read()
 
 	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	canny = cv2.Canny(gray,100,200)
 
-	image, contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-	cont = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
+	# image, contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+	# cont = cv2.drawContours(frame, contours, -1, (0,255,0), 3)
 	cv2.imshow('Canny', canny)
-	cv2.imshow("Contours", cont)
+	# cv2.imshow('Frame', frame)
+	cv2.imshow('Gray', gray)
+	# cv2.imshow("Contours", cont)
 	
 	# cv2.setMouseCallback('frame', click_pos)
 
