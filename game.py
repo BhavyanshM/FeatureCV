e5b37ff93e50eeeb69f96e9b7780d3c119af065f
BhavyanshM <bhavyanshmishra@gmail.com>
2019-02-19 13:25:35 -0600

    Add the correct webcam number

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ def click_pos(event, x, y, flags, param):
 		print(frame[y,x])
 
 #The camera
-cap = cv2.VideoCapture(1);
+cap = cv2.VideoCapture(0);
 
 while True:
 	# cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
